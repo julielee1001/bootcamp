a41b812841cd80bc554a483896a7bf6557248729
Julie Lee <julielee@Julies-MacBook-Air.local>
2024-11-19 22:28:44 -0600

    added devise authentication and confirmable migration

--- ruby/homework/blog/app/controllers/authors_controller.rb
+++ ruby/homework/blog/app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
-    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+    before_action :authenticate_user!, except: [:index, :show]
 
     def index
         @authors = Author.all
